b4dde9c1cef3fb0b714f4ca30cccc53b19ea7b4a
Janhawi K <kjanhawi357@gmail.com>
2025-09-05 03:01:43 +0530

    fix: access driver routes under /users/driver only

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import UserController from '../controllers/UserController.js'
 import { sendResponse } from "../middlewares/sendResponse.js";
 import authenticateUser from "../middlewares/auth/authenticateUser.js";
+import driverRouter from "./DriverRoute.js";
 
 const userController = new UserController(); 
 const userRouter = express.Router();
@@ -11,5 +12,7 @@ userRouter.post('/login', userController.loginUser, sendResponse);
 userRouter.post('/getme', authenticateUser, userController.getUserProfile, sendResponse);
 userRouter.post('/update', authenticateUser, userController.updateUserProfile, sendResponse);
 userRouter.post('/change-pass', authenticateUser, userController.changeUserPassword, sendResponse);
+
+userRouter.use("/driver", driverRouter);
 
 export default userRouter;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import userRouter from "./UserRoute.js";
-import driverRouter from "./DriverRoute.js";
 
 const indexRouter = Router();
 
 indexRouter.use('/users', userRouter);
-indexRouter.use("/driver", driverRouter);
 
 export default indexRouter;
+
 
