2a4789e4f5e0dc99940f258981130410b58cd38a
Janhawi K <kjanhawi357@gmail.com>
2025-09-05 12:25:46 +0530

    changed driver routes

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -2,7 +2,6 @@ import express from "express";
 import UserController from '../controllers/UserController.js'
 import { sendResponse } from "../middlewares/sendResponse.js";
 import authenticateUser from "../middlewares/auth/authenticateUser.js";
-import driverRouter from "./DriverRoute.js";
 
 const userController = new UserController(); 
 const userRouter = express.Router();
@@ -12,7 +11,5 @@ userRouter.post('/login', userController.loginUser, sendResponse);
 userRouter.post('/getme', authenticateUser, userController.getUserProfile, sendResponse);
 userRouter.post('/update', authenticateUser, userController.updateUserProfile, sendResponse);
 userRouter.post('/change-pass', authenticateUser, userController.changeUserPassword, sendResponse);
-
-userRouter.use("/driver", driverRouter);
 
 export default userRouter;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import userRouter from "./UserRoute.js";
+import driverRouter from "./DriverRoute.js";
 
 const indexRouter = Router();
 
 indexRouter.use('/users', userRouter);
+userRouter.use("/driver", driverRouter);
 
 export default indexRouter;
 
